156b854680e9a9a95cde75359058acec9f77d6b2
lucastdcj <lucastdcj@gmail.com>
2017-07-23 19:32:20 -0300

    Update MARIO_2P.cpp

--- MARIO_2P.cpp
+++ MARIO_2P.cpp
@@ -11,13 +11,13 @@ int main() {
   while (scanf(" %d %d", &n, &m) == 2 && n + m) {
     for (int i = 0; i < m; i++) scanf(" %d", &v[i]);
     int ans = n;
-    for (int i = 0, ni = i; i < m; i++) {
-      while (ni < m && v[ni] - v[i] + 1 <= n) ni++;
-      ans = min(ans, n - ((ni - 1) - i + 1));
+    for (int i = 0, pos = i; i < m; i++) {
+      while (pos < m && v[pos] - v[i] + 1 <= n) pos++;
+      ans = min(ans, n - ((pos - 1) - i + 1));
     }
-    for (int i = m - 1, ni = i; i >= 0; i--) {
-      while (ni >= 0 && v[i] - v[ni] + 1 <= n) ni--;
-      ans = min(ans, n - (i - (ni + 1) + 1));
+    for (int i = m - 1, pos = i; i >= 0; i--) {
+      while (pos >= 0 && v[i] - v[pos] + 1 <= n) pos--;
+      ans = min(ans, n - (i - (pos + 1) + 1));
     }
     printf("%d\n", ans);
   }
