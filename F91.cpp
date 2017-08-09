892124d41d9de92c2bcd0131a07d7c2571c243dd
lucastdcj <lucastdcj@gmail.com>
2017-08-09 16:29:10 -0300

    Create F91.cpp

---
+++ F91.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define MAXN 2010010
+
+int pd[MAXN];
+
+int f91(int x) {
+  // usando o & passa
+  int& ans = pd[x];
+  if (ans != -1) return ans;
+  
+  if (x <= 100) ans = f91(f91(x + 11));
+  else ans = x - 10;
+  return ans;
+}
+
+int main() {
+  int n;
+  memset(pd, -1, sizeof(pd));
+  while (scanf(" %d", &n) == 1 && n) {
+    printf("f91(%d) = %d\n", n, f91(n));
+  }
+  return 0;
+}
